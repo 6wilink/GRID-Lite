23209a8957e3577be6a24806fd349656da624a7e
qz <zhaoqige@163.com>
2017-02-08 18:21:41 -0500

    Demo scripts "lib/web/*.lua"

---
+++ lib/web/basic.lua
@@ -0,0 +1,18 @@
+-- basic information
+-- by Qige
+-- 2017.01.05
+
+basic = {}
+
+function basic.update()
+  local _dev = {}
+  
+  _dev.model = "ARN4433P8D"
+  _dev.sn = "1701CNBJ02003"
+  _dev.mac = "AB:CD:EF:00:01:03"
+  _dev.ip = "192.168.1.24/255.255.255.0"
+  
+  return _dev
+end
+
+return basic
---
+++ lib/web/nw.lua
@@ -0,0 +1,32 @@
+-- system strings
+-- by Qige
+-- 2017.01.06/2017.01.09
+
+require 'wui.base.cmd'
+require 'wui.base.seed'
+
+
+nw = {}
+
+nw.cmdTotal = 'ifconfig br-lan | grep bytes | grep :[0-9]* -o | grep [0-9]* -o | tr "\n" ","'
+
+function nw.update()
+  local _data = {}
+  
+  local _dev1 = { peer = 'AB:CD:EF:00:01:03', remote = 'AB:CD:EF:00:01:04', signal = -75 + seed.random(0, 10), noise = -101, rx = '10.05 Mbps (4s)', tx = '10.05 Mbps (4s)', inactive = seed.random(0, 1024) }
+  local _dev2 = { peer = 'AB:CD:EF:00:01:03', remote = 'AB:CD:EF:00:01:05', signal = -75 + seed.random(0, 10), noise = -101, rx = '10.05 Mbps (4s)', tx = '10.05 Mbps (4s)', inactive = seed.random(0, 1024) }
+  
+  table.insert(_data, _dev1)
+  table.insert(_data, _dev2)
+  
+  return _data
+end
+
+function nw.demo()
+  local _total = seed.random(0, 26)
+  local _lan = 0.384 * _total
+  local _wlan = (1 - 0.384) * _total
+  return { total = seed.random(0, 26), wan = 0, lan = _lan, wlan = _wlan }
+end
+
+return nw
