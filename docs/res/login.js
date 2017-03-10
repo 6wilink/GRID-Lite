231a394cbe81db0d99c28054356dc6fec180694c
zhaoqige <zhaoqige@users.noreply.github.com>
2017-03-10 11:46:00 -0500

    Try "GitHub" pages, add "/docs/" (#31)

---
+++ docs/res/login.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function(){
+	$('.modal').modal();
+	$('.qz-doing').hide();
+
+	$('#qz-form-login').submit(function() {
+		$('.qz-doing').show();
+
+		var user = $('#username').val();
+		var pass = $('#password').val();
+
+		if (user.length < 5 || pass.length < 5) {
+			$('.qz-doing').hide();
+			var $toastContent = $('<span>Please check your USERNAME & PASSWORD</span>');
+			Materialize.toast($toastContent, 3000);
+			return false;
+		}
+		$('.qz-doing').hide();
+	});
+
+	$('#qz-owner').click(function() {
+		$('#username').val('admin');
+		$('#password').val('admin');
+		$('#qz-form-login').trigger('submit');
+	});
+});
