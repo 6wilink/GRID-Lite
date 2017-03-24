667e18d91ea249c67aba567ddae2923efcdc0012
qz <zhaoqige@163.com>
2017-03-24 15:53:21 -0400

    Merge "qz-grid" into "zh-cn"

--- lib/grid/base/user.lua
+++ lib/grid/base/user.lua
@@ -2,26 +2,30 @@
 -- by Qige
 -- 2016.04.05/2017.01.03/2017.01.04/2017.01.06
 
-require 'grid.base.conf'
-require 'grid.base.file'
-require 'grid.base.cmd'
-require 'grid.base.cgi'
+local conf = require 'six.conf'
+local file = require 'six.file'
+local cmd = require 'six.cmd'
+local cgi = require 'grid.base.cgi'
 
-user = {}
+local user = {}
+
+user.default = {}
+user.default._session = '/tmp/.grid_remote'
+user.default.conf = 'grid-lite'
+user.default.username = 'root'
+user.default.password = '6Harmonics'
 
 user.conf = {}
-user.conf.file = conf.default.file
-
 function user.conf.session()
-  local f = conf.uci.get(user.conf.file, 'v1', 'id')
+  local f = conf.file.get(user.default.conf, 'v1', 'id')
   if (f and f ~= '') then
     return f
   end
-	return conf.default.user.session
+	return user.default._session
 end
 
 function user.conf.root()
-	local ip = conf.uci.get(user.conf.file, 'v1', 'root')
+	local ip = conf.file.get(user.default.conf, 'v1', 'root')
   if (ip and ip ~= '') then
     return ip
   end
@@ -29,55 +33,53 @@ function user.conf.root()
 end
 
 function user.conf.user()
-	local _user = conf.uci.get(user.conf.file, 'v1', 'user')
+	local _user = conf.file.get(user.default.conf, 'v1', 'user')
   if (_user and _user ~= '') then
     return _user
   end
-  return conf.default.user.username
+  return user.default.username
 end
 
 function user.conf.passwd()
-	local _passwd = conf.uci.get(user.conf.file, 'v1', 'pass')
+	local _passwd = conf.file.get(user.default.conf, 'v1', 'pass')
   if (_passwd and _passwd ~= '') then
     return _passwd
   end
-  return conf.default.user.password
+  return user.default.password
 end
 
 user.data = {}
 user.data._root = user.conf.root()
 user.data._user = user.conf.user()
-user.data._passwd = user.conf.passwd()
+user.data._password = user.conf.passwd()
 user.data._session = user.conf.session()
 
 
 -- if user logged in, compare saved ip with peer ip
 user.verify = {}
-function user.verify.remote()
-	local remote = cgi.data.remote()
+function user.verify.Remote(_remote)
   local _file = user.data._session
 	local _ip = file.read(_file)
-	if (_ip and remote) then
-		return (remote == _ip)
+	if (_ip and _remote) then
+		return (_remote == _ip)
 	end
   --return true -- TODO: DEBUG USE ONLY
 	return false
 end
 
-function user.verify.login(_user, _passwd)
-	if (_user and _passwd and _user == user.data._user and _passwd == user.data._passwd) then
+function user.verify.Login(_user, _password)
+	if (_user and _password and _user == user.data._user and _password == user.data._password) then
     return true
   end
   return false
 end
 
 user.ops = {}
-function user.ops.save()
-	local remote = cgi.data.remote()
+function user.ops.Save(_remote)
   local _file = user.data._session
-  file.write(_file, remote)
+  file.write(_file, _remote)
 end
-function user.ops.logout()
+function user.ops.Logout()
   local _file = user.data._session
   file.write(_file, '\n')
 end
